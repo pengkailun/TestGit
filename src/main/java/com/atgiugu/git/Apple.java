d672af5116fef91b5aa158ed3c9107644b14d6d4
tom_gld <goodMorning@qq.com>
2019-10-01 09:58:00 +0800

    dfgdf

--- src/main/java/com/atgiugu/git/Apple.java
+++ src/main/java/com/atgiugu/git/Apple.java
@@ -2,12 +2,9 @@ package com.atgiugu.git;
 
 public class Apple {
 	
-	public String ss() {
+	public void ss() {
 		System.out.println("sdfsd");
-		return null;
-		
-		System.out.println("sdfsdfdd"); System.out.println("sdfsd");
-		
+		System.out.println("hot_fix");
 	}
 
 }
